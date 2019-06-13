055f3c2fa2e28f9d1cc8b1df8f29bdf218deb75b
ravidyarev <ravidya_rev@yahoo.com>
2019-06-13 10:22:51 +0530

    Updated write to DB

--- now_playing_movie.py
+++ now_playing_movie.py
@@ -2,68 +2,51 @@ import http.client
 import json
 import requests
 import sqlite3
+#import pandas as pd
 
 #Create a DB to save movie info
 dbname='now_playing_movie_db,sqllite'
-conn = sqlite3.connect(str(dbname))
-cur=conn.cursor()
+dbconn = sqlite3.connect(str(dbname))
+cur=dbconn.cursor()
+#cur.execute('''DROP TABLE Movie_List ''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Movie_List 
+(Movie_Id INTEGER, Movie_Title Text, Release_Country Text )''')
 
 #fetch data from TMDb
 conn = http.client.HTTPSConnection("api.themoviedb.org")
 
-country_list=['US']#,'UK','IE','AU','CA']
+country_list=['US','UK','IE','AU','CA']
 
 for i in country_list:
-  #print(i)
-
+  #fetch currently playing movies
   payload = "{}"
-
   conn.request("GET", "/3/movie/now_playing?page=1&language=en-US&api_key=bbb0e77b94b09193e6f32d5fac7a3b9c&region="+i, payload)
-
   res = conn.getresponse()
   now_playing_data = res.read()
-
   now_playing_parsed = json.loads(now_playing_data)
   
-  #to get director info
-  payload = "{}"
-  conn.request("GET", "/3/movie/320288/credits?api_key=bbb0e77b94b09193e6f32d5fac7a3b9c", payload)
+  #Save the fetched data to DB
+  j=0
+  for k in now_playing_parsed['results']:
+    if now_playing_parsed['results'][j]['id']!='N/A':
+      movie_id = int(now_playing_parsed['results'][j]['id'])
+    if now_playing_parsed['results'][j]['title']!='N/A':
+      movie_title = now_playing_parsed['results'][j]['title'] 
+    j=j+1
+  
+    cur.execute('SELECT Movie_Id,Movie_Title,Release_Country FROM Movie_List WHERE Movie_Id = ?', (movie_id,))
+    row = cur.fetchone()
 
-  res = conn.getresponse()
-  credits_data = res.read()
+    if row is None:
+      cur.execute('''INSERT INTO Movie_List (Movie_Id,Movie_Title,Release_Country)
+            VALUES (?,?,?)''', (movie_id,movie_title,str(i)))
+ 
 
-  credits_parsed = json.loads(credits_data)
+#Validate results save
+cur.execute('SELECT Movie_Id,Movie_Title,Release_Country FROM Movie_List')
+print(cur.fetchall())
 
-  #print(json.dumps(parsed, indent=4, sort_keys=True))
+#print (pd.read_sql_query("SELECT * FROM Movie_List", dbconn))
 
-  #with open('data'+i+'.json', 'w') as outfile:
-  #  json.dump(parsed, outfile)
-
-#print(parsed['results'][0]['id'])
-#Create DB with sqllite
-
-
-# Goes through the json dataset and extracts information if it is available
-if parsed['results'][0]['id']!='N/A':
-    movie_id = int(now_playing_parsed['results'][0]['id'])
-    movie_title = now_playing_parsed['results'][0]['title']
-    movie_genre = now_playing_parsed['results'][0]['genre_ids']
-
-print(movie_genre)
-# SQL commands
-cur.execute('''DROP TABLE Movie_List''')
-
-cur.execute('''CREATE TABLE IF NOT EXISTS Movie_List 
-(Movie_Id INTEGER, Movie_Title Text )''')
-
-cur.execute('SELECT Movie_Id,Movie_Title FROM Movie_List WHERE Movie_Id = ? ', (movie_id,))
-row = cur.fetchone()
-
-if row is None:
-    cur.execute('''INSERT INTO Movie_List (Movie_Id,Movie_Title)
-            VALUES (?,?)''', (movie_id,movie_title,))
-else:
-    print("Record already found. No update made.")
-
-cur.execute('SELECT Movie_Id,Movie_Title FROM Movie_List WHERE Movie_Id = ? ', (movie_id,))
-print(cur.fetchone())
+dbconn.commit()
+dbconn.close()
